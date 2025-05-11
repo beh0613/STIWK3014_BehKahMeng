6c402de1909a62ea23b1485ed27d5cf3a4d46965
beh0613 <your.email@example.com>
2025-05-11 22:53:24 +0800

    Real-Time Programming

---
+++ ThreadStates/src/main/java/org/Week_04/ThreadNew.java
@@ -0,0 +1,14 @@
+package org.Week_04;
+
+/**
+ * Hello world!
+ *
+ */
+public class ThreadNew
+{
+    public static void main( String[] args )
+    {
+      Thread t = new Thread();
+      System.out.println(t.getState());
+    }
+}
---
+++ ThreadStates/src/main/java/org/Week_04/ThreadRunnable.java
@@ -0,0 +1,9 @@
+package org.Week_04;
+
+public class ThreadRunnable {
+    public static void main(String [] args){
+        Thread t = new Thread();
+        t.start();
+        System.out.println(t.getState());
+    }
+}
